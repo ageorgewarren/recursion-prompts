d678460fa2e4f5c0ffcf016f553d94defa154d37
ageorgewarren <ageorgewarren@gmail.com>
2020-12-26 03:31:00 -0500

    Completed through 10 palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -125,6 +125,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+    string = string.toLowerCase();
+    string = string.replace(" ","")
+    if ( string === "" ) return true
+    if ( string.slice( 0 , 1 ) !== string.slice( -1 )) {
+        return false
+    } else {
+       return palindrome(string.slice( 1 , -1 ))
+    } 
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
