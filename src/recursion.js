efcf9afac3b79ffbc148557467a471702115bcdc
ageorgewarren <ageorgewarren@gmail.com>
2020-12-27 03:13:22 -0500

    Completed through 19 fizzBuzz

--- src/recursion.js
+++ src/recursion.js
@@ -227,6 +227,15 @@ var buildList = function(value, length) {
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = function(n) {
+    if ( n === 0 ) return []
+    else {
+        var arr = fizzBuzz( n - 1 )
+        if ( n % 3 === 0 && n % 5 !== 0) arr.push( 'Fizz' )
+        else if ( n % 3 !== 0 && n % 5 === 0 ) arr.push( 'Buzz' )
+        else if ( n % 3 === 0 && n % 5 === 0 ) arr.push( 'FizzBuzz' )
+        else arr.push( n.toString() )
+        return arr;
+    }
 };
 
 // 20. Count the occurence of a value in a list.
